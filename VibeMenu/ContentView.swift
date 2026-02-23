b19edca966e77184dfb23b76284925e44e7bf3ec
ajie5419 <ajie5419@gmail.com>
2026-02-23 10:00:17 +0800

    Initial Commit

---
+++ VibeMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VibeMenu
+//
+//  Created by 赵小杰 on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VibeMenu/VibeMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeMenuApp.swift
+//  VibeMenu
+//
+//  Created by 赵小杰 on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct VibeMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
